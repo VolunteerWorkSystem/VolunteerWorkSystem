7068afcfa00334635c3ccad3adb44b0a8131ede7
LJlkdskdjflsa <liliangjya@gmail.com>
2024-12-08 16:52:19 +0800

    feat: renew

--- frontend/src/admin/qrcodes/index.tsx
+++ frontend/src/admin/qrcodes/index.tsx
@@ -1,4 +1,3 @@
-
 import { Box, Grid2, Typography } from '@mui/material';
 import { QRCodeSVG } from 'qrcode.react';
 
@@ -14,11 +13,13 @@ export function Qrcodes() {
     }}>
       <Typography variant="h2" fontWeight={'bold'} textAlign={'center'} gutterBottom>出勤一般志工</Typography>
       <Grid2 container >
-        <Grid2 size={6} display="flex" justifyContent="center" alignItems="center">
-          <QRCodeSVG size={280} value="checkin-23h457ge9toutbg4eh9fso" />,
+        <Grid2 size={6} display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+          <Typography variant="h4" gutterBottom>簽到</Typography>
+          <QRCodeSVG size={280} value="checkin-23h457ge9toutbg4eh9fso" />
         </Grid2>
-        <Grid2 size={6} display="flex" justifyContent="center" alignItems="center">
-          <QRCodeSVG size={280} value="checkout-getipokenjth54kgrkbfr" />,
+        <Grid2 size={6} display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+          <Typography variant="h4" gutterBottom>簽退</Typography>
+          <QRCodeSVG size={280} value="checkout-getipokenjth54kgrkbfr" />
         </Grid2>
 
       </Grid2>
